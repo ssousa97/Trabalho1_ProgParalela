75436f3798798b667ef5f32a5e5eab7aa1dbabf3
Cainã Figueiredo <cainafigueiredo@poli.ufrj.br>
2021-09-09 13:18:36 -0300

    Correção no loop mais externo

--- tarefa01/run.sh
+++ tarefa01/run.sh
@@ -1,26 +1,31 @@
 # Definindo os parametros que serao avaliados
 N_values="1000000000 10000000000 100000000000"
 num_procs="1 2 4 8 12 16"
-programs="pi_monteCarlo_paralelo.c pi_trapezio_paralelo.c"
+programs="pi_monteCarlo_paralelo pi_trapezio_paralelo"
 
 num_experiments=$(($(wc -w <<< ${N_values})*$(wc -w <<< ${num_procs})))
 cur_experiment=1
 
 # Definindo algumas constantes
 PBS_SCRIPTS_DIR="./pbs-scripts"
-JOBS_RESULTS_DIR="${PBS_SCRIPTS_DIR}/jobs-results"
+JOBS_RESULTS_DIR="./jobs-results"
 
 # Removendo diretorios de execucoes anteriores
 rm -r ${PBS_SCRIPTS_DIR}
+rm -r ${JOBS_RESULTS_DIR}
 
 # Construindo os diretorios para esta execucao
-mkdir ${PBS_SCRIPTS_DIR}
-mkdir ${JOBS_RESULTS_DIR}
+mkdir -p ${PBS_SCRIPTS_DIR}
+mkdir -p ${JOBS_RESULTS_DIR}
 
-for program in programs; do
+for program in ${programs}; do
 
     # Compilando o programa
-    mpicc ./src/${program} -o ./bin/${program} 
+    mpicc ./src/${program}.c -o ./bin/${program} 
+
+    # Criando os diretórios para o programa
+    mkdir ${PBS_SCRIPTS_DIR}/${program}
+    mkdir ${JOBS_RESULTS_DIR}/${program}
 
     for N in ${N_values}; do
         for np in ${num_procs}; do
@@ -37,7 +42,7 @@ for program in programs; do
 
 # Arquivo de saida:
 #PBS -j oe
-#PBS -o ${JOBS_RESULTS_DIR}/${name}.out
+#PBS -o ${JOBS_RESULTS_DIR}/${program}/${name}.out
 
 # Tempo de execução do b
 #PBS -l walltime=3:00:00
@@ -50,11 +55,11 @@ for program in programs; do
 
 exit 0"
 
-            echo "${script}" >> "${PBS_SCRIPTS_DIR}/${name}.sh"
+            echo "${script}" >> "${PBS_SCRIPTS_DIR}/${program}/${name}.sh"
 
             # Submetendo job
 
-            qsub ${PBS_SCRIPTS_DIR}/${name}.sh
+            qsub ${PBS_SCRIPTS_DIR}/${program}/${name}.sh
 
             read -p "Pressione qualquer tecla para submeter o proximo job..."
             cur_experiment=$((cur_experiment+1))
